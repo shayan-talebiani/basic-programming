54a70cac6dead857065c73171a18ee57f296e4bf
shayan-talebiani <shayant94@gmail.com>
2025-12-17 19:27:56 +0330

    section 4 problem 4

--- section 4/problem 4/main.c
+++ section 4/problem 4/main.c
@@ -2,6 +2,9 @@
 
 void set_a(int n, int a[n][n]);
 
+int calculate_state_count(int n, int a[n][n]);
+void set_using_state(int n, int a[n][n], int using_state[n], int index);
+
 int main(){
 
     int n;
@@ -9,6 +12,10 @@ int main(){
 
     int a[n][n];
     set_a(n, a);
+
+    int state_count = calculate_state_count(n, a);
+
+    printf("%d", state_count);
 
     return 0;
 }
@@ -23,9 +30,54 @@ void set_a(int n, int a[n][n]){
             int conections;
             scanf("%d", &conections);
 
+            a[i][j] = conections;
+
         }
     
     }
     
+
+}
+
+int calculate_state_count(int n, int a[n][n]){
+
+    int using_state[n];
+    int state_count = 0;
+
+    for (int i = 0; i < n; i++){
+        using_state[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++){
+
+        if (using_state[i] == 0){
+            
+            state_count += 1;
+
+            using_state[i] = 1;
+
+            set_using_state(n, a, using_state, i);
+
+        }
+        
+    }
+    
+
+    return state_count;
+}
+
+void set_using_state(int n, int a[n][n], int using_state[n], int index){
+    
+    for (int i = 0; i < n; i++){
+
+        if (using_state[i] == 0 && a[index][i] > 0){
+
+            using_state[i] = 1;
+
+            set_using_state(n, a, using_state, i);
+
+        }
+        
+    }
 
 }
