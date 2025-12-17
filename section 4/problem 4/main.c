f3f7900de1730932ed43128238ebcf3f072201df
shayan-talebiani <shayant94@gmail.com>
2025-12-17 14:58:13 +0330

    section 4 problems 4

---
+++ section 4/problem 4/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void set_a(int n, int a[n][n]);
+
+int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    int a[n][n];
+    set_a(n, a);
+
+    return 0;
+}
+
+
+void set_a(int n, int a[n][n]){
+
+    for (int i = 0; i < n; i++){
+        
+        for (int j = 0; j < n; j++){
+            
+            int conections;
+            scanf("%d", &conections);
+
+        }
+    
+    }
+    
+
+}
