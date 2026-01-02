da2048775b80afb307f1469ba33203bdf02f5797
shayan-talebiani <shayant94@gmail.com>
2026-01-02 14:00:14 +0330

    section 5 problem 2

---
+++ section 5/problem 2/func.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+    struct Node *left;     
+    struct Node *right;
+} Node;
+
+Node* build_structure(int n);
+void build_structure_tree(Node *answer, int l, int r);
+
+Node* build_structure(int n) {
+    // your code
+
+    Node *answer = (Node *)malloc(sizeof(Node));
+    build_structure_tree(answer, 1, n);
+
+    return answer;
+}
+
+void build_structure_tree(Node *answer, int l, int r){
+
+    if (l == r){
+        answer->value = l;
+        return;
+    }
+    
+    int v = (r + l) / 2;
+
+    answer->value = v;
+
+    if (l <= v - 1){
+
+        answer->left = (Node *)malloc(sizeof(Node));
+        build_structure_tree(answer->left, l, v - 1);
+
+    }
+
+    if (v + 1 <= r){
+
+        answer->right = (Node *)malloc(sizeof(Node));
+        build_structure_tree(answer->right, v + 1, r);
+    
+    }
+
+}
